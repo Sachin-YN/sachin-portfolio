f9fc8c35c58fc7c8ebc80e42f7ef57b29ef8bc69
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-20 11:23:37 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
-};
+    // (optional) add other PostCSS plugins here, e.g.:
+    // 'postcss-import': {},
+    // 'postcss-nested': {}
+  }
+}
