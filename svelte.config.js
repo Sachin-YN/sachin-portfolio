598159386f08d1f24dd30f8948c655f2717199f4
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-23 11:38:14 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-export default {
-  preprocess: preprocess({
-    postcss: true
-  }),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess({ postcss: true }),
   kit: {
-    adapter: adapter(),
-    target: '#svelte'
+    adapter: adapter()
   }
 };
+
+export default config;
