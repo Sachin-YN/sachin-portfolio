b0d536fd4b8561b2157b51627d99d0b9529630e5
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-30 12:49:27 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Navbar from './Navbar'
 
 export default function Layout({ children, title = 'Sachin Yoganandham' }) {
+  const currentYear = new Date().getFullYear()
+
   return (
     <>
       <Head>
@@ -10,11 +12,23 @@ export default function Layout({ children, title = 'Sachin Yoganandham' }) {
         <meta name="description" content="Portfolio of Sachin Yoganandham, Data Analyst" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Site Header */}
       <Navbar />
-      {/* main content sits over the fixed background */}
-      <main className="pt-20 relative z-10">
+
+      {/* Main Content */}
+      <main className="relative z-10 pt-20">
         {children}
       </main>
+
+      {/* Footer */}
+      <footer className="mt-16 py-6 bg-gray-800 text-center text-gray-400">
+        <div className="max-w-6xl mx-auto px-4">
+          <p className="text-sm">
+            &copy; {currentYear} Sachin Yoganandham. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </>
   )
 }
