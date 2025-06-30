24201a24467b3768cb7f5867132b598a9bc3c937
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-30 10:47:52 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../styles/globals.css'  // ← must import your global CSS
+import '../styles/globals.css'   // must import global styles
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
