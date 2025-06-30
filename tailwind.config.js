035d20dd72ad504c21f3cc9cf51b737fd3adb14c
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-30 09:39:25 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+// tailwind.config.js
 module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-blue': '#00ffff',
+        'neon-pink': '#ff00ff',
+        'neon-green': '#39ff14',
+        'neon-orange': '#ff4500',
+      },
+    },
   },
   plugins: [],
 }
