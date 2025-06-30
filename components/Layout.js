382198bcc8bd44ff2ed8c40e2d9e401df1d3276f
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-30 10:41:13 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,17 +2,19 @@
 import Head from 'next/head'
 import Navbar from './Navbar'
 
-export default function Layout({ children, title = 'Sachin Yadav' }) {
+export default function Layout({ children, title = 'Sachin Yoganandham' }) {
   return (
     <>
       <Head>
         <title>{title} | Portfolio</title>
-        <meta name="description" content="Portfolio of Sachin Yadav, Data Analyst" />
+        <meta name="description" content="Portfolio of Sachin Yoganandham, Data Analyst" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      {/* pt-20 gives enough space for the fixed navbar */}
-      <main className="pt-20">{children}</main>
+      {/* pt-20 offsets the fixed navbar; no fixed height so page can scroll */}
+      <main className="pt-20">
+        {children}
+      </main>
     </>
   )
 }
