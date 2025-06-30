7c99266dc82529bb35b54ec94e46c3a1e7e3e192
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-06-30 10:22:56 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        navy: '#0a1f44',
-        'electric-blue': '#00b7eb',
-        'sky-blue': '#66d9ef',
+        'silver-light': '#f5f5f5',
+        'silver':       '#c0c0c0',
+        'silver-dark':  '#808080',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
