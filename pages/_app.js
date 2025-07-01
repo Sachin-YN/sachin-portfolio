62dc5633d5604380f72f349959a215d260ba66b6
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-07-01 22:31:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,28 @@
 // pages/_app.js
 import '../styles/globals.css'    // 🔥 This line is required
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const router = useRouter()
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
+  useEffect(() => {
+    if (!GA_ID) return
+
+    const handleRouteChange = (url) => {
+      window.gtag('config', GA_ID, {
+        page_path: url,
+      })
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events, GA_ID])
+
   return <Component {...pageProps} />
 }
+
+export default App
