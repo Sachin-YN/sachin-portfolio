7c9a360e3b7575425185c892a84530a1f1a4b3e5
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-07-01 15:09:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
     extend: {
+      colors: {
+        accent: '#00ffff',
+        'accent-dark': '#00d4d4',
+      },
       backdropBlur: {
         xs: '2px',
         sm: '4px',
