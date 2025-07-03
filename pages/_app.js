08cb38554269239416e512965a7e08b28714bfac
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-07-03 16:38:41 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,25 @@
 import '../styles/globals.css'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useRouter } from 'next/router'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={router.pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      <Toaster position="bottom-center" />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </>
   )
 }
