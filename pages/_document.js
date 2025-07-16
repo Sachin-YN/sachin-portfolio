cc623e00812ceda65765c1282979353e1ceeeda7
Sachin Yoganandham <46930204+Sachin-YN@users.noreply.github.com>
2025-07-16 09:17:08 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Google AdSense Meta Tag for verification */}
+        <meta name="google-adsense-account" content="ca-pub-8612329739653505" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
